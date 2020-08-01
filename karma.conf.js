879b08a271606a1e6b5f9cdafb146d087260aa4d
Kolesov Anton <kolesov.anton.s@gmail.com>
2020-08-01 15:50:09 +0300

    Rename project

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/shopping-list'),
+      dir: require('path').join(__dirname, './coverage/budgetadvisor'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
