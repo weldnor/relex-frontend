01ddb68b22f3bf561b2dd2efff91906f0fa5e6a7
Kolesov Anton <kolesov.anton.s@gmail.com>
2020-08-09 17:44:33 +0300

    Add groups model/service

---
+++ src/app/features/groups/models/groups.model.ts
@@ -0,0 +1,9 @@
+export interface ExistingGroup {
+  id: number;
+  groupName: string;
+  groupDescription: string;
+  createdAt: string;
+  createdBy: number;
+  joinCode: string;
+  balance: number;
+}
---
+++ src/app/features/groups/services/groups.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from '../../../../environments/environment';
+import {ExistingGroup} from '../models/groups.model';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupService {
+  constructor(private readonly http: HttpClient) {
+  }
+
+  getAllGroups(): Observable<ExistingGroup[]> {
+    return this.http.get<ExistingGroup[]>(`${environment.api}/groups/`);
+  }
+}
